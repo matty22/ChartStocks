ea00db2a34aaba4499d98cf6cde2daad3a5ef36d
Matt <matt.leonard.22@gmail.com>
2017-09-18 15:43:16 -0700

    got socket.io setup and runnning

---
+++ sockets/socket.js
@@ -0,0 +1,9 @@
+module.exports = function (io) {
+  io.on('connection', function(socket) { 
+    console.log('user connected');
+    socket.on('disconnect', function(){
+      console.log('user disconnected');
+    })
+    });
+  
+}
