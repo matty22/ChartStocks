ddfa42cf92c851d23f0e09ad00f9f236f526e19d
Matt <matt.leonard.22@gmail.com>
2017-09-19 19:44:43 -0700

    little bit of clean up and mucking about with styles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,22 +3,22 @@
 let alphaAdvantageKey = 'MCVCJGWR900F07S6';
 
 // Function runs on page load to populate chart with at least 1 stock ticker
-window.onload = function() {
-  let symbol = 'MSFT';
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 
-                  symbol + '&outputsize=compact&apikey=' + alphaAdvantageKey, true);
-  xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
-  xhr.onload = function() {
-      if (xhr.status === 200) {
-        // console.log(xhr.responseText);
-      }
-      else {
-          console.error("you suck: API request function");
-      }
-  }
-  xhr.send();
-}
+// window.onload = function() {
+//   let symbol = 'MSFT';
+//   let xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + 
+//                   symbol + '&outputsize=compact&apikey=' + alphaAdvantageKey, true);
+//   xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
+//   xhr.onload = function() {
+//       if (xhr.status === 200) {
+//         // console.log(xhr.responseText);
+//       }
+//       else {
+//           console.error("you suck: API request function");
+//       }
+//   }
+//   xhr.send();
+// }
 
 
 function search() {
@@ -74,7 +74,8 @@ function buildChart(datesArray, dataArray) {
             label: 'AAPL',
             data: dataArray,
             borderWidth: 1,
-            fill: false
+            fill: false,
+            borderColor: '#ff0000'
           }]
         }
       }
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,6 @@
 module.exports = function (io) {
   io.on('connection', function(socket) { 
     socket.on('search result', function(search){
-      // console.log("content in socket is: " + search);
       io.emit('search result', search);
     });
   }); 
